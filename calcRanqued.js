40832037c3a1e1902e7c1ebb08fa994f933b7ea9
WandsonB <wandson.study@gmail.com>
2024-02-05 00:03:56 -0300

    Added vector for ranks

--- calcRanqued.js
+++ calcRanqued.js
@@ -3,21 +3,23 @@ function balance (wins,defeat) {
     return numbalance
 }
 
+let classHero = ["Ferro", "Bronze", "Prata", "Ouro", "Diamante", "Lendário", "Imortal"]
+
 function patent(rank){
     if (rank <= 10){
-        console.log(`O Herói tem de saldo de ${rank} está no nível de Ferro`)
+        console.log(`O Herói tem de saldo de ${rank} está no nível de ${classHero[0]}`)
     }else if (rank >= 11 && rank <=20){
-        console.log(`O Herói tem de saldo de ${rank} está no nível de Bronze`)
+        console.log(`O Herói tem de saldo de ${rank} está no nível de ${classHero[1]}`)
     }else if (rank >= 21 && rank <=50){
-        console.log(`O Herói tem de saldo de ${rank} está no nível de Prata`)
+        console.log(`O Herói tem de saldo de ${rank} está no nível de ${classHero[2]}`)
     }else if (rank >= 51 && rank <=80){
-        console.log(`O Herói tem de saldo de ${rank} está no nível de Ouro`)
+        console.log(`O Herói tem de saldo de ${rank} está no nível de ${classHero[3]}`)
     }else if (rank >= 81 && rank <=90){
-        console.log(`O Herói tem de saldo de ${rank} está no nível de Diamante`)
+        console.log(`O Herói tem de saldo de ${rank} está no nível de ${classHero[4]}`)
     }else if (rank >= 91 && rank <=100){
-        console.log(`O Herói tem de saldo de ${rank} está no nível de Lendário`)
+        console.log(`O Herói tem de saldo de ${rank} está no nível de ${classHero[5]}`)
     }else if (rank >=101){
-        console.log(`O Herói tem de saldo de ${rank} está no nível de Imortal`)
+        console.log(`O Herói tem de saldo de ${rank} está no nível de ${classHero[6]}`)
     }
 }
 

3935e789664919c479efafc2e26fc15c48ec8ce5
WandsonB <wandson.study@gmail.com>
2024-02-05 00:09:40 -0300

    Added function dialog

--- calcRanqued.js
+++ calcRanqued.js
@@ -3,23 +3,27 @@ function balance (wins,defeat) {
     return numbalance
 }
 
-let classHero = ["Ferro", "Bronze", "Prata", "Ouro", "Diamante", "Lendário", "Imortal"]
+let rankHero = ["Ferro", "Bronze", "Prata", "Ouro", "Diamante", "Lendário", "Imortal"]
+
+function dialog(numBalance, vectorRankHero){
+    console.log(`O Herói tem de saldo de ${numBalance} está no nível de ${vectorRankHero}`)
+}
 
 function patent(rank){
     if (rank <= 10){
-        console.log(`O Herói tem de saldo de ${rank} está no nível de ${classHero[0]}`)
+        dialog(rank, rankHero[0])
     }else if (rank >= 11 && rank <=20){
-        console.log(`O Herói tem de saldo de ${rank} está no nível de ${classHero[1]}`)
+        dialog(rank, rankHero[1])
     }else if (rank >= 21 && rank <=50){
-        console.log(`O Herói tem de saldo de ${rank} está no nível de ${classHero[2]}`)
+        dialog(rank, rankHero[2])
     }else if (rank >= 51 && rank <=80){
-        console.log(`O Herói tem de saldo de ${rank} está no nível de ${classHero[3]}`)
+        dialog(rank, rankHero[3])
     }else if (rank >= 81 && rank <=90){
-        console.log(`O Herói tem de saldo de ${rank} está no nível de ${classHero[4]}`)
+        dialog(rank, rankHero[4])
     }else if (rank >= 91 && rank <=100){
-        console.log(`O Herói tem de saldo de ${rank} está no nível de ${classHero[5]}`)
+        dialog(rank, rankHero[5])
     }else if (rank >=101){
-        console.log(`O Herói tem de saldo de ${rank} está no nível de ${classHero[6]}`)
+        dialog(rank, rankHero[6])
     }
 }
 
